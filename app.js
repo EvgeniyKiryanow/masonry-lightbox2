4943887d157c5d74d3e48ae51d4bbaf73c6c07cc
Evgeniy Kirianov <85870790+EvgeniyKiryanow@users.noreply.github.com>
2021-10-31 11:42:47 +0200

    add global preloader

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(window).load(function() {
-    $(".loader").delay(1000).fadeOut("slow");
-    $(".overlayer").delay(1000).fadeOut("slow");
+    $(".loader").delay(1500).fadeOut("slow");
+    $(".overlayer").delay(1500).fadeOut("slow");
 })
 
 $(".grid").imagesLoaded(function() {
