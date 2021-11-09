3a43c7982dd34ca15c165b56aff10361be9e73b3
Evgeniy Kirianov <85870790+EvgeniyKiryanow@users.noreply.github.com>
2021-11-09 14:39:58 +0200

    push methods to load

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 $(window).load(function() {
+    $(".grid").imagesLoaded(function() {
+        $(".grid").masonry({
+            itemSelector: ".grid-item"
+        });
+    });
+    lightbox.option({
+        'wrapAround': true,
+        'fadeDuration': 500,
+        'imageFadeDuration': 500,
+    })
     $(".loader").delay(1500).fadeOut("slow");
     $(".overlayer").delay(1500).fadeOut("slow");
-})
-
-$(".grid").imagesLoaded(function() {
-    $(".grid").masonry({
-        itemSelector: ".grid-item"
-    });
-});
-
-lightbox.option({
-    'wrapAround': true,
-    'fadeDuration': 500,
-    'imageFadeDuration': 500,
 })
